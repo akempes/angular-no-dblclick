15cd15011d47c03d6219e1bd0579a6cf75ba6506
Arjan Kempes <arjankempes@Arjans-MacBook-Pro.local>
2015-07-24 02:45:14 +0200

    Basics added

--- example/example.js
+++ example/example.js
@@ -5,7 +5,21 @@ var myApp = angular.module('exampleApp',['noDblclick']);
 myApp.controller('ExampleController', ['$scope', function( $scope ){
     'use strict';
 
+    	$scope.test1 = function () {
+    		console.log('test1');
+    	};
 
+    	$scope.test2 = function () {
+    		console.log('test2');
+    	};
+
+    	$scope.unlock = function  () {
+    		$scope.$broadcast('noDblclick.unlock', 1);
+    	}
+    	
+    	$scope.unlockall = function  () {
+    		$scope.$broadcast('noDblclick.unlock');
+    	}
 
     }
 ]);
--- src/no-dblclick.js
+++ src/no-dblclick.js
@@ -1,81 +1,140 @@
 var module = angular.module('noDblclick', ['ng']);
 
 module
-.directive('noDblclick', [ "$compile", "$timeout", function( $compile, $timeout ) {
+.service('noDblclickService', ['$rootScope', function ( $rootScope ) {
+
+    var setState = (function (id, state) {
+        if(this.elements[id].isDisabled!==state){
+            this.elements[id].isDisabled = state;
+        }
+    }).bind(this);
+
+    this.elements = {};
+
+    this.lock = function (id) {
+        if( id === undefined ){
+            Object.keys(this.elements).map((function (key) {
+                setState(this.elements[key].id, true);
+            }).bind(this));
+        }else{
+            setState(id, true);
+        }
+    };
+
+    this.unlock = function (id) {
+        if( id === undefined ){
+            Object.keys(this.elements).map((function (key) {
+                setState(this.elements[key].id, false);
+            }).bind(this));
+        }else{
+            setState(id, false);
+        }
+    };
+
+    $rootScope.$on('noDblclick.unlock', function(event, id) {
+        this.unlock(id);
+    });
+    $rootScope.$on('noDblclick.lock', function(event, id) {
+        this.lock(id);
+    });
+
+    $rootScope.noDblclickService = this;
+
+}])
+.directive('noDblclick', [ 'noDblclickService', '$compile', function( noDblclickService, $compile ) {
 	'use strict';
 
 		// I augment the template element DOM structure before linking.
         function compile( tElement ) {
 
-            var sublink,
-            ngClick = tElement.attr("ng-click");
-            tElement.attr( "ng-disabled", "is_disabled" );
-            tElement.attr( "ng-click", (ngClick || '')+";disableNoDbClickBtn()" );
+            var 
+            sublink,
+            obj = {
+                id: Object.keys(noDblclickService.elements).length,
+                domId: tElement.attr('id') || '',
+                el: tElement,
+                isDisabled: false
+            },
+            ngClick = (tElement.attr('ng-click') || '').replace(/\s/g, ''),
+            ngDblclick = (tElement.attr('ng-dblclick') || '').replace(/\s/g, ''),
+            ngDisabled = (tElement.attr('ng-disabled') || '').replace(/\s/g, '');
+
+
+            // Add object to service
+            noDblclickService.elements[obj.id] = obj;
+          
+
+            // Add disableMe function to ng-click
+            if(ngClick!=='' && ngClick.indexOf(';', ngClick.length - 1) < 0){
+                ngClick += ';';
+            }
+            ngClick += 'noDblclickService.lock('+obj.id+')';
+
+
+            // prevent double click
+            if(ngDblclick!=='' && ngDblclick.indexOf(';', ngDblclick.length - 1) < 0){
+                ngDblclick += ';';
+            }
+            ngDblclick = 'return -1';
+
+
+            // Add isDisabled expression to ng-disabled
+            if(ngDisabled!==''){
+                ngDisabled = '('+ngDisabled+') || ';
+            }
+            ngDisabled += 'noDblclickService.elements['+obj.id+'].isDisabled';
+
+
+            // Set new attributes
+            tElement.attr( 'ng-click', ngClick);
+            tElement.attr( 'ng-dblclick', ngDblclick);
+            tElement.attr( "ng-disabled", ngDisabled);
 
             sublink = $compile( tElement, null, 1500 );
 
             /*jslint unparam:true */
             function link( $scope, element, attributes, modelCtrl, transclude ) {
 
-                $scope.is_disabled = false;
-                var dbClickEvent = $scope.$on('noDbClick.unlock', function(event, id) {
-                    if(!id || element.attr('id')===id){
-                        $timeout(function () {
-                            $scope.is_disabled = false;
-                        });
-                    }
-                });
+                console.log($scope);
 
-                $scope.disableNoDbClickBtn = function () {
-                    $timeout(function () {
-                        $scope.is_disabled = true;
-                    });
-                };
+                var garbase = []; 
 
+                // Add listeners to current scope
+                garbase.push($scope.$on('noDblclick.unlock', function(event, id) {
+                    noDblclickService.unlock(id);
+                }));
+                garbase.push($scope.$on('noDblclick.lock', function(event, id) {
+                    noDblclickService.lock(id);
+                }));
+
+
+                // Destroy listeners
                 $scope.$on('$destroy', function() {
-                    if(dbClickEvent) {
-                        dbClickEvent();
-                    }
+                    angular.forEach(garbage, function (listener) {
+                        listener();
+                    });
                 });
 
-                // Because we are using the ISOLATE scope, in this case, we have
-                // to transclude the content. If we don't do this, then the call
-                // to $compile() above and sublink() below will end up linking
-                // the element CONTENT to the ISOLATE scope, which will break
-                // our references. So, instead, what we have to do is allow the
-                // content to be transcluded and linked to the outer scope
-                // (outside of our directive).
+
                 transclude(
                     function( content ) {
-
                         element.append( content );
-
                     }
                 );
 
-                // Link the compiled directives that we dynamically added to the
-                // current element. This will also link any directives that were
-                // already on the element, but were at a lower priority.
-                // --
-                // NOTE: We probably we want to do this after the transclude()
-                // since a directive is supposed to be able to rely on the DOM
-                // of its child content.
                 sublink( $scope );
 
             }
+            /*jslint unparam:false */
 
             return link;
 
         }
-        /*jslint unparam:false */
 
 	return {
         compile: compile,
         priority: 1500,
         restrict: "A",
-        // scope: {
-        //     isolateFriend: "=bnFriend"
-        // },
         terminal: true,
         transclude: true
 	};
